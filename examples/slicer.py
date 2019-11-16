6d7744d5aef4c85dd4dffec6a1590278133606d2
Damo1884 <damien.murphy37@mail.dcu.ie>
2019-11-15 18:07:29 -0800

    update args

--- examples/slicer.py
+++ examples/slicer.py
@@ -5,27 +5,43 @@ from pyvox.models import Vox
 from skimage.util.shape import view_as_blocks
 import math  
 
-m1 = VoxParser('./models/64x64x64.vox').parse()
-print('Vox Slicer')
-newSize = 32
+if len(sys.argv) < 4:
+	print('Error: You must pass the input filename, output folder & slice size')
+	print('Usage: python slicer.js <input filename> <output folder> <slice size>');
+	print('eg. python slicer.js input.vox output.vox 32');
+else:
+	print('Vox Slicer Starting...')
+	inputFile = sys.argv[1]
+	outputPath = sys.argv[2]
+	sliceSize = int(sys.argv[3])
+	print('Input File: ', inputFile)
+	print('Output Path: ', outputPath)
+	print('Slice Size: ', sliceSize)
+	m1 = VoxParser(inputFile).parse()
+	
+	data = m1.to_dense()
+	originalSize = len(data)
 
-data = m1.to_dense()
-originalSize = len(data)
-chunks = view_as_blocks(data, block_shape=(newSize, newSize, newSize))
-outputCount = math.ceil((originalSize/newSize)*(originalSize/newSize)*(originalSize/newSize))
-count = int(originalSize/newSize)
+	print('Input Vox Size: ', originalSize)
+	print('Output Vox Slice Size: ', sliceSize)
 
-print('Input Size: ', originalSize)
-print('Output Size: ', newSize)
-print('Creating ', outputCount, ' Vox Files')
+	if originalSize % sliceSize != 0:
+		print('Error: Input Vox size must be a multiple of Output Slice size')
+	else:
+		chunks = view_as_blocks(data, block_shape=(sliceSize, sliceSize, sliceSize))
+		outputCount = math.ceil((originalSize/sliceSize)*(originalSize/sliceSize)*(originalSize/sliceSize))
+		count = int(originalSize/sliceSize)
 
-for z in range(0,count):
-	for y in range(0,count):
-		for x in range(0,count):
-			print('chunk: ', x, y, z)
-			chunk = chunks[x][y][z]
-			vox = Vox.from_dense(chunk)
-			filename = './out/'+str(x)+'-'+str(y)+'-'+str(z)+'.vox'
-			VoxWriter(filename, vox).write()
-			print('Created:', filename)
-print('done')
+		
+		print('Creating ', outputCount, ' Vox Files')
+
+		for z in range(0,count):
+			for y in range(0,count):
+				for x in range(0,count):
+					print('chunk: ', x, y, z)
+					chunk = chunks[x][y][z]
+					vox = Vox.from_dense(chunk)
+					filename = outputPath + '/'+str(x)+'-'+str(y)+'-'+str(z)+'.vox'
+					VoxWriter(filename, vox).write()
+					print('Created:', filename)
+		print('done')
